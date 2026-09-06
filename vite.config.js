fc742d3fb237f91c77503fb62e780cc9b062e1a0
Jason Lien 連宗聖 <jasonlien@starlux-airlines.com>
2026-09-06 15:47:29 +0800

    fix: serve site from custom domain root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/wu2.0/',
+  base: '/',
   plugins: [vue()],
   build: {
     rollupOptions: {
